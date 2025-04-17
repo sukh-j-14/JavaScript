54dbb4869ec3b90c0db9d8d151687d992d756ea0
sukh_j_14 <sukhit4108@gmail.com>
2025-04-17 19:52:01 +0530

    committed: 01_basics/conversionsandoperations.js

--- 01_basics/conversionsandoperations.js
+++ 01_basics/conversionsandoperations.js
@@ -12,3 +12,21 @@ console.log(valueInNumber);
 let isLoggedIn=undefined;
 let booleanisLoggedIn = Boolean(isLoggedIn);
 console.log(booleanisLoggedIn);
+
+let value= 3;
+let negValue = -value;
+
+console.log(negValue);
+
+let str1= "hello";
+let str2= " world";
+let str3= str1+str2;
+console.log(str3);
+
+console.log(1+"2");
+console.log(1+2+"3");
+console.log("1"+2+3);
+
+console.log(+true);
+console.log(+"");
+
