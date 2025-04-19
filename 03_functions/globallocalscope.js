2d2ddbceb1a7d977747d0e6b30fd326a00d80b7c
sukh_j_14 <sukhit4108@gmail.com>
2025-04-19 23:10:41 +0530

    updated

--- 03_functions/globallocalscope.js
+++ 03_functions/globallocalscope.js
@@ -1,11 +1,35 @@
-// var c=300;
-let a =300;
-if (true){
-    let a=10;
-    const b=20;
-    console.log(a);
-    // c=30;
+// // var c=300;
+// let a =300;
+// if (true){
+//     let a=10;
+//     const b=20;
+//     console.log(a);
+//     // c=30;
+// }
+// console.log(a);
+// // console.log(b);
+// // console.log(c);
+
+//nested scope
+function one(){
+    const userName="Sukh";
+    function two(){
+        const website="youtube";
+        console.log(userName);
+    }
+    // console.log(website);
+    two();
 }
-console.log(a);
-// console.log(b);
-// console.log(c);
+one();
+ 
+//+++++++++++++++++ interesting +++++++++++++++++
+addOne(2); //Here, it doesn't give error
+function addOne(num){
+    return num+1;
+}
+addOne(5);
+// addTwo(5); //here , it gives error
+const addTwo= function(num){
+    return num+2;
+}
+addTwo(5);
