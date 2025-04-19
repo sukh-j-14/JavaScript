2560e88238e91423209132931eab5a3ef600a50a
sukh_j_14 <sukhit4108@gmail.com>
2025-04-19 22:51:08 +0530

    functions done

--- 03_functions/functions.js
+++ 03_functions/functions.js
@@ -1,6 +1,7 @@
 function sayMyName(){
     console.log("Sukh");
 }
+//function calls
 sayMyName();//CALL
 
 function addTwoNumbers(number1 , number2){
@@ -8,7 +9,7 @@ function addTwoNumbers(number1 , number2){
 }
 const Result=addTwoNumbers(3,5);
 console.log(Result);
-
+//return statement
 function addTwoNumbers1(number1 , number2){
     return number1+number2;
 }
@@ -25,3 +26,29 @@ function loginUserMessage(username="sam"){
 }
 console.log(loginUserMessage("Sukh"));
 console.log(loginUserMessage());
+
+function calculateCartPrice(val1,val2,...num){
+    return num;
+}
+console.log(calculateCartPrice(100,200,300,500));
+//Handling Objects
+const user={
+    userName:"Sukh",
+    price:199
+}
+function handleObject(anyobject){
+    console.log(`UserName is ${anyobject.userName} and price is ${anyobject.price}`);
+}
+
+handleObject(user);
+handleObject({
+    userName: "Sukhpreet Singh",
+    price: 399,
+});
+//Handling array
+const array=[1,2,3,4,5,6]
+console.log(returnArray(array));
+function returnArray(getArray){
+    return getArray[2];
+}
+
