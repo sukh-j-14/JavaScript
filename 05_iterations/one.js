8e7f17d64579f1a0f02d8c9cef1dc18b9935fe73
sukh_j_14 <sukhit4108@gmail.com>
2025-04-21 00:19:08 +0530

    loopsbasic

---
+++ 05_iterations/one.js
@@ -0,0 +1,25 @@
+
+// for(let index=0; index<10;index++){
+//     const element =index;
+//     console.log(element);
+// }
+//break and continue
+// for (let i=0;i<21;i++){
+//     // console.log(i);
+//     if(i==5){
+//         break; //continue
+//     }
+//     console.log(i);
+// }
+// let index=0;
+// while(index<10){
+//     console.log(index);
+//     index++;
+// }
+let score=1;
+do{
+    console.log(`Score is ${score}`);
+    score++;
+}
+while (score<=10);
+console.log(score);
