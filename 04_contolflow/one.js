1542ba636b0060c95caa05a1f3f52692bbc3198d
sukh_j_14 <sukhit4108@gmail.com>
2025-04-20 23:34:48 +0530

    ifelse

---
+++ 04_contolflow/one.js
@@ -0,0 +1,31 @@
+// const isUserLoggedIn=true;
+// if(isUserLoggedIn){
+
+// }
+//<,>,<=,>=,==,===,!=
+if(2=="2"){    //true
+    console.log("executed");
+}
+if(2==="2"){
+    console.log("Executed strict");
+}
+const score=200;
+if(score<100){
+    const power="fly";
+    console.log(`User power: ${power}`);
+}
+// console.log(`User power: ${power}`);
+
+const balance= 1000;
+// if (balance>500) console.log("test"), console.log("test2");
+
+// if(balance<500){
+//     console.log("less than 500")
+// } else if(){
+
+// }
+
+// const isUserLoggedIn=true;
+
+// ||,&&
+
