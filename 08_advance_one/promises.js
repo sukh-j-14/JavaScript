eafd89de09fe3942ba786dc3d40f68cd2dc3ace0
sukh_j_14 <sukhit4108@gmail.com>
2025-07-08 00:10:54 +0530

    comments change

--- 08_advance_one/promises.js
+++ 08_advance_one/promises.js
@@ -1,112 +1,112 @@
-// // fetch('https://api.github.com/users/sukh-j-14').then().catch().finally();
+// fetch('https://api.github.com/users/sukh-j-14').then().catch().finally();
 
 
-// // Above is consuming promises 
+// Above is consuming promises 
 
-// const promiseOne= new Promise(function(resolve,reject){
-//     // Do an async task
-//     // DB calls, cryptography, network
-//     setTimeout(function(){
-//         console.log('Async task is complete')
-//         resolve();                                                  // call resolve to connect with .then
-//     },2000);
-// });                                                                 // promise bnana
+const promiseOne= new Promise(function(resolve,reject){
+    // Do an async task
+    // DB calls, cryptography, network
+    setTimeout(function(){
+        console.log('Async task is complete')
+        resolve();                                                  // call resolve to connect with .then
+    },2000);
+});                                                                 // promise bnana
 
-// promiseOne.then(function(){
-//     console.log("Promise Consumed");
-// })
+promiseOne.then(function(){
+    console.log("Promise Consumed");
+})
 
 
-// // new Promise( (resolve,reject) => {
-// //     setTimeout(()=>{
-// //         console.log("Async Task 2");
-// //         resolve();
-// //     },1000)
-// // }).then(()=>{
-// //     console.log("Async 2 resolved");
-// // });
-
-// new Promise(function(resolve, reject){
-//     setTimeout(function(){
-//         console.log("Async task 2");
-//         resolve()
-//     }, 1000)
-
-// }).then(function(){
-//     console.log("Async 2 resolved");
-// })
-
-// const promiseThree= new Promise(function(resolve,reject){
-//     setTimeout(function(){
-//         resolve({
-//             userName: "Chai",
-//             email : "test@example.com"
-//         })
-//     },1000)
-// })
-
-// promiseThree.then(function(user){
-//     console.log(user);
-// })
-
-// const PromiseFour= new Promise(function(resolve,reject){
-//     setTimeout(function(){
-//         let error=true;
-//         if(!error) {
-//             resolve(
-//                 {
-//                 userName : "Sukh-j-14",
-//                 password : "12345678"
-//                 }
-//             )
-//         } else{
-//             reject('ERROR: Something went wrong');
-//         }
-//     },500)
-// });
-
-// PromiseFour
-// .then(function(user){
-//     return user.userName;
-// })
-// .then(function(username){
-//     console.log(username);
-// })
-// .catch(function(error){
-//     console.log(error);
-// })
-// .finally(function(){
-//     console.log("Kuch toh ho hi gya hai promise ka");
-// })
-
-// const promiseFive= new Promise((resolve,reject)=>{
+// new Promise( (resolve,reject) => {
 //     setTimeout(()=>{
-//         let error=true;
-//         if(!error) {
-//             resolve(
-//                 {
-//                 userName : "javascript",
-//                 password : "123"
-//                 }
-//             )
-//         } else{
-//             reject('ERROR: JS went wrong');
-//         }
+//         console.log("Async Task 2");
+//         resolve();
 //     },1000)
+// }).then(()=>{
+//     console.log("Async 2 resolved");
 // });
 
-// // let's use other method for handling like asyncawait
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve()
+    }, 1000)
 
-// async function consumePromiseFive(){
-//     try{
-//         const response= await promiseFive;                 // error mtlb reject ko khud se handle krlo with try catch
-//         console.log(response);
-//     } catch(error){
-//         console.log(error);
-//     }
-// }
+}).then(function(){
+    console.log("Async 2 resolved");
+})
 
-// consumePromiseFive();
+const promiseThree= new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({
+            userName: "Chai",
+            email : "test@example.com"
+        })
+    },1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+const PromiseFour= new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error=true;
+        if(!error) {
+            resolve(
+                {
+                userName : "Sukh-j-14",
+                password : "12345678"
+                }
+            )
+        } else{
+            reject('ERROR: Something went wrong');
+        }
+    },500)
+});
+
+PromiseFour
+.then(function(user){
+    return user.userName;
+})
+.then(function(username){
+    console.log(username);
+})
+.catch(function(error){
+    console.log(error);
+})
+.finally(function(){
+    console.log("Kuch toh ho hi gya hai promise ka");
+})
+
+const promiseFive= new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        let error=true;
+        if(!error) {
+            resolve(
+                {
+                userName : "javascript",
+                password : "123"
+                }
+            )
+        } else{
+            reject('ERROR: JS went wrong');
+        }
+    },1000)
+});
+
+// let's use other method for handling like asyncawait
+
+async function consumePromiseFive(){
+    try{
+        const response= await promiseFive;                 // error mtlb reject ko khud se handle krlo with try catch
+        console.log(response);
+    } catch(error){
+        console.log(error);
+    }
+}
+
+consumePromiseFive();
 
 // async function getAllUsers(){
 //     try{
