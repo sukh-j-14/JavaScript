8fbca3deb95508de6554afcd1173589eb5215d93
sukh_j_14 <sukhit4108@gmail.com>
2025-07-07 23:44:17 +0530

    learnt .then .catch .finally

--- 08_advance_one/promises.js
+++ 08_advance_one/promises.js
@@ -36,3 +36,47 @@ new Promise(function(resolve, reject){
     console.log("Async 2 resolved");
 })
 
+const promiseThree= new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({
+            userName: "Chai",
+            email : "test@example.com"
+        })
+    },1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+const PromiseFour= new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error=true;
+        if(!error) {
+            resolve(
+                {
+                userName : "Sukh-j-14",
+                password : "12345678"
+                }
+            )
+        } else{
+            reject('ERROR: Something went wrong');
+        }
+    },500)
+});
+
+PromiseFour
+.then(function(user){
+    return user.userName;
+})
+.then(function(username){
+    console.log(username);
+})
+.catch(function(error){
+    console.log(error);
+})
+.finally(function(){
+    console.log("Kuch toh ho hi gya hai promise ka");
+})
+
+
